6dd97ad564c32b2a6766fbd649a8e4534dca797f
kickscar <kickscar@gmail.com>
2016-10-17 16:22:40 +0900

    cookie 세팅

--- src/com/bit2016/mysite/action/main/MainAction.java
+++ src/com/bit2016/mysite/action/main/MainAction.java
@@ -1,8 +1,10 @@
 package com.bit2016.mysite.action.main;
 
 import java.io.IOException;
+import java.net.HttpCookie;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +15,12 @@ public class MainAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie c = new Cookie( "nickname", "baboya" );
+		c.setDomain( "localhost" );
+		c.setPath( "/mysite3" );
+		c.setMaxAge( 24 * 60 * 60 );
+		response.addCookie( c );
+		
 		WebUtil.forward(
 			request, 
 			response, 
